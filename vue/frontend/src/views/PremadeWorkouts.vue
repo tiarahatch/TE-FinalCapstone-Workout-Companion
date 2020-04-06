407a633272150935176d75bf22c0a0f896c14e43
Justin Stevenson <justin.stevenson036@gmail.com>
2020-04-06 18:20:56 -0400

    Final Capstone Day One Progress

---
+++ vue/frontend/src/views/PremadeWorkouts.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Please browser our premade-workouts</h1>
+   <!-- <p>You must be authenticated to see this</p> -->
+
+        
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'premade-workouts'
+}
+</script>
---
+++ vue/frontend/src/views/Schedule.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Upcoming Events</h1>
+   <!-- <p>You must be authenticated to see this</p> -->
+
+        
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'schedule'
+}
+</script>
