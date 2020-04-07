e1797768bf98806742e26dbfdf2a4f4dca7d4837
David Greenzalis <dgreenzalis@gmail.com>
2020-04-07 13:22:45 -0400

    push to break into individual tasks

--- vue/frontend/src/views/Schedule.vue
+++ vue/frontend/src/views/Schedule.vue
@@ -2,7 +2,14 @@
   <div>
     <h1>Upcoming Events</h1>
    <!-- <p>You must be authenticated to see this</p> -->
-
+    <div>
+      <button @click = 'created'/>
+      <ul>
+        <li v-for='item in schedule' v-bind:key='item'>
+            {{item.className}}
+        </li>
+      </ul>
+    </div>
         
   </div>
 </template>
@@ -15,11 +22,22 @@ export default {
  
  data(){
   return{
-    apiURL:"",
     schedule: [],
 
   };
 },
+methods:{
+  created() {
+    fetch(`${process.env.VUE_APP_REMOTE_API}/api/schedule`)
+      .then((response)=>{
+        return response.json();
+      })
+      .then((json)=>{
+       this.schedule = json;
+       console.log(json);
+      })
+  }
+}
 
 }
 
