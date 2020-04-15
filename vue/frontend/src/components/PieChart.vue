5eaf159fa0582d2ddddec7bf6f98346cd6be3dc8
Justin Stevenson <justin.stevenson036@gmail.com>
2020-04-15 19:08:50 -0400

    CHARTS

---
+++ vue/frontend/src/components/PieChart.vue
@@ -0,0 +1,36 @@
+<script>
+import {Doughnut} from 'vue-chartjs'
+export default {
+extends: Doughnut,
+name: 'category-pecentage',
+    data: () => ({
+        chartdata: {
+            labels: ['Cardio', 'Strength', 'Flexibility', 'CrossFit'],
+            datasets: [
+                {
+                    label: 'Data',
+                    data: [40, 20, 20, 20],
+                    backgroundColor: [
+                        '#f57ff5',
+                        '#678fcf',
+                        '#62d962',
+                        '#ed9880'
+                    ]
+                },
+         
+            ]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    }),
+mounted() {
+    this.renderChart(this.chartdata, this.options)
+    }
+}
+</script>
+
+<style>
+
+</style>
