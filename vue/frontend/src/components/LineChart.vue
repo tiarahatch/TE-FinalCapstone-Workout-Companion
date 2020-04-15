a391e4005db36d61e21ad85949e40b7c318d955a
Justin Stevenson <justin.stevenson036@gmail.com>
2020-04-15 19:57:13 -0400

    Dashboard

---
+++ vue/frontend/src/components/LineChart.vue
@@ -0,0 +1,39 @@
+<script>
+import {Line} from 'vue-chartjs'
+
+export default {
+
+extends: Line,
+name: 'workout-consistancy',
+    data: () => ({
+        chartdata: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            datasets: [
+                {
+                    label: 'Completed Workouts',
+                    data: [1, 3, 2, 5, 0, 6, 4, 7, 2, 0, 1, 2],
+                    pointBackgroundColor: '#ffffff',
+                    pointBorderColor: '#045499',
+                    borderColor: '#000000',
+                    backgroundColor: '#045499',
+                    pointRadius: 5,
+                       
+                    
+                },
+         
+            ]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    }),
+mounted() {
+    this.renderChart(this.chartdata, this.options)
+    }
+}
+</script>
+
+<style>
+
+</style>
--- vue/frontend/src/views/Dashboard.vue
+++ vue/frontend/src/views/Dashboard.vue
@@ -1,14 +1,17 @@
 <template>
 
 <div id="wholepage">
-    <h1>MY DASHBOARD</h1>
+    <div class='navButtons'>
+    <button v-on:click='logout'> LogOut</button>
+    </div>
+      <h1>MY DASHBOARD</h1>
     <h2>Welcome Back</h2>
     <div class='side'>
         <router-link to='/premade-workouts' tag='button'>Choose Workout</router-link><br>
         <router-link to='/schedule' tag='button'>Sign Up for Class</router-link><br>
         <router-link to='/NewUser' tag='button'>Edit Profile</router-link><br>
         <button>My Metrics</button><br>
-        <button v-on:click='logout'> LogOut</button>
+        
         </div>
 
     <div class='images'>
@@ -16,6 +19,7 @@
        
     </div>
   <category-percentage id='Doughnut'></category-percentage>
+  <workout-consistancy id='line'></workout-consistancy>
 </div>
 </template>
 
@@ -23,9 +27,11 @@
 <script>
 import auth from '@/auth'
 import CategoryPercentage from '@/components/PieChart.vue'
+import WorkoutConsistancy from '@/components/LineChart.vue'
 export default {
   components: {
-      CategoryPercentage
+      CategoryPercentage,
+      WorkoutConsistancy
   },
 methods: {
     logout() {
@@ -43,7 +49,7 @@ methods: {
 
 @import url('https://fonts.googleapis.com/css2?family=Oswald&display=swap');
 
-.side {
+.side button {
  position: absolute;
     top: 100px;
     left: 200px;
@@ -76,9 +82,9 @@ height: 30%;
   width: 35%;
   cursor: pointer;
   background-color: rgba(0, 0, 0, 0.75);
-  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
-}
-h1{
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19); 
+ }
+h1 {
     text-align: center;
     font-family: 'Black Ops One';
     color: white;
@@ -112,9 +118,39 @@ h2 {
 
 #Doughnut {
     position: absolute;
-    top: 50%;
-    right: 20%;
+    top: 16%;
+    right: 36%;
     height: 35%;
 }
-  
+  #line {
+      position: absolute;
+      height: 40%;
+      width: 95%;
+      top: 60%;
+      left: 3%;
+  }
+ .navButtons{
+    display:flex;
+    justify-content: flex-end;
+    margin-bottom:1%;
+    
+}
+.navButtons button{
+    width: 10%;
+    height: 30px; 
+    background-color: rgba(0, 0, 0, 0.5);
+    color:white;
+    font-family: 'Oswald', sans-serif;
+    border: none;
+    transition: .6s;
+}
+
+.navButtons button:hover {
+  width: 12%;
+  height: 30px;
+  cursor: pointer;
+  background-color: rgba(0, 0, 0, 0.75);
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+}
+
 </style>
